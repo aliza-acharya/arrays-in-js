6579971fffee1a6565f4f1a4756f5c32dffbda15
aliza-acharya <acharyaaliza79@gmail.com>
2025-02-09 14:26:50 +0545

    first commit

---
+++ alija.js
@@ -0,0 +1,129 @@
+// looping
+// for(let i=0; i<12; i++){
+//     console.log(i);
+// }
+// console.log('aliza acharya');
+// for (let i = 1; i <= 10; i++)
+// {
+//     console.log(i);
+// }
+// let sum = 0;
+// let i = 1;
+// while (i <=100){
+//     sum += i;
+//     i++;
+// }
+// console.log("the sum of number from 1 to 100 is: ", sum);
+
+// 
+// let sum = 0;
+// let i = 1;
+// while (i <=100){
+// if (i % 2!=0){
+//     sum +=i;
+// }
+// i++;
+// }
+// console.log("the sum of odd numbers from 1 to 100 is:", sum);
+// create an array storing value of multiple datatypes
+// let mixedData = [
+//     [1, "John", true],                
+//     [2, "Mary", false],               
+//     [3, "Chris", { age: 30, city: "NY" }] 
+//   ];
+  
+  
+//   console.log(mixedData[0][1]);        
+//   console.log(mixedData[2][2].city);    
+//   console.log(mixedData[1][2]);        
+//   let a = ["saujal", 15, true];
+//   let b = ["js", "python","java"];
+//   console.log(typeof a);
+// const name = "alija";
+// let age= 22;
+// const isStudent = true;
+// let hobbies = ["reading", "cycling", "gaming"];
+// let favouriteColors = ["blue", "green", "red"];
+// let profile = {
+//     name: name,
+//     age: age,
+//     isStudent: isStudent,
+//     favouriteColors: favouriteColors,
+//     hobbies: hobbies,
+//     contact:{ 
+//         email: "acharyaaliza79@gmail.com",
+//     phone: "980-435-0092"
+//     }
+// };
+
+// console.log("Name:", profile.name);
+// console.log("Age:", profile.age);
+// console.log("Is a student", profile.isStudent);
+// console.log("\nfavourite colors:");
+// for(let color of profile.favouriteColors){
+// console.log("- " + color);
+// }
+// console.log("\nhobbies:");
+// for(let hobbies of profile.hobbies);{
+//     console.log("> " + hobbies);
+// }
+// console.log("contact Email:", profile.contact.email);
+
+// let a = [1,2,3,4,5,6,7,8];
+// console.log(a[3]);
+// changing value inside the array
+// let a = [0,1,2,3,[0,1,2,3],4,5,6,[0,1,2,3]];
+// a[4][2] = 20;
+// console.log(a);
+
+// unshift
+// let a = [1,2,3,4,5,6,7,8,9];
+// a.unshift(3);
+// console.log(a)
+
+// shift
+// let a = [1,2,3,4,5,6];
+// a.shift();
+// console.log(a);
+
+// Push
+// let a = [1,2,3,4,5,6];
+// a.push(91);
+// console.log(a);
+
+// pop
+// let a = [1,2,3,4,5,6];
+// a.pop();
+// console.log(a);
+
+// index of 
+// let a = [1,2,3,4,5,6];
+
+// console.log(a.indexOf(4));
+
+// let a = [1,2,3,4,5,6];
+// console.log(a[3]);
+
+// tostring
+// let a = [1,2,3,4,5,6];
+// a.toString();
+// console.log(a);
+
+// // includes
+
+// let a = [1,2,3,4,5,6];
+// console.log(a.includes(9));
+
+// let b = [1,2,3,4];
+// let c = b.includes(2);
+// console.log(c);
+
+// concat 
+// let a = [1,2,3,4,5];
+// let b = [6,7,8,9];
+// console.log(a.concat(b));
+
+// join 
+// let a = [1,2,3,4,5];
+// let b = [6,7,8,9];
+// console.log(a.join(" "));
---
+++ script.js
@@ -0,0 +1,161 @@
+// let a = 5;
+// let b = 4;
+// let c = 9;
+// console.log(a&&b);
+// console.log(a||c);
+// console.log(a+b);
+// console.log(c-a);
+// console.log(b*c);
+// console.log(c/a);
+// console.log(c == a);
+// ram="he is a good boy";
+// console.log(ram);
+// let n = 10;
+// if(n>10) {
+//     console.log("it is greater than 10");
+// }
+// else{
+//     console.log("it is lesser than 10");
+// }
+// WAP to check whetther you can vote or not
+// let age=22;
+// if(age<18){
+//     console.log("you are not eligible to caste a vote");
+// }
+// else {
+//       console.log("you are eligible to caste a vote");
+// }
+// wap to check whether you are Child, adult or old 
+// let age=30;
+// if(age<15){
+//     console.log("you are child");
+// }
+// else if(age>15 && age<56){
+//     console.log("you are adult");
+// }
+// else{
+//     console.log("you are old");
+// }
+// odd or even 
+// let x=24;
+// if(x%2==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+// let a=9;
+// switch(a){
+//     case 1:
+//         console.log("sunday");
+//         break;
+//     case 2:
+//         console.log("monday");
+//         break;
+//         case 3:
+//             console.log("tuesday");
+//             break;
+//             case 4:
+//             console.log("wednesday");
+//             break;
+//             case 5:
+//             console.log("thursday");
+//             break;
+//             case 6:
+//             console.log("friday"); 
+//             break;
+//             case  7:
+                //             console.log("saturday"); 
+//             break;
+//                 default:
+//                 console.log("it is invalid");  
+// }
+
+// postive/negative
+// let n=6;
+// if(n>0) {
+//     console.log("this is postive");
+// }
+// else if(n==0){
+//     console.log("it is zero");
+// }
+// else{
+//     console.log("it is negative");
+// }
+
+// season 
+
+// let season= "winter" ;
+// switch(season){
+//     case "summer":
+//         console.log("it is summer");
+//         break;
+//         case "winter":
+//             console.log("it is winter");
+//             break;
+//             case "rainy":
+//         console.log("it is rainy"); 
+//         break;
+                   
+//          default:
+//         console.log("it is invalid");            
+// }
+
+// let n=35;
+// if(n%6==0){
+//     console.log("this is divisible by 6");
+// }
+// else{
+//     console.log("it is not divided by 6");
+// }
+// // campare two variable to determine which one is greater. 
+// let a=3;
+// let b=1;
+// if(a>b){
+//     console.log("the number a is greater than b");
+// }
+// else{
+//     console.log("the number b is greater than a");
+// }
+// using switch to print the name of the months according to the first alpphabet
+// let a="j";
+// switch(a){
+//         case "b":
+//             console.log("baisakh");
+//             break;
+//             case "j":
+//             console.log("jestha");
+//             break;
+//             case "A":
+//             console.log("Asar");
+//             break;
+//             case "s":
+//             console.log("shrawan");
+//             break;
+//             case "v":
+//             console.log("bhadra");
+//             break;
+//             case "as":
+//             console.log("ashwin");
+//             break;
+//             case "K":
+//             console.log("kartik");
+//             break;
+//             case "m":
+//             console.log("mangsir");
+//             break;
+//             case "p":
+//             console.log("poush");
+//             break;
+//             case "ma":
+//             console.log("magh");
+//             break;
+//             case "f":
+//             console.log("falgun");
+//             break;
+//             case "c":
+//             console.log("chaitra");
+//             break;
+//             default:
+//                 console.log("it is invalid");
+// }
